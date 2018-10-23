ea23de37aa8c113e39c63f9ee0a28ea23a27f5be
Prateek Gogia <gogia.prateek@hotmail.com>
2018-10-23 07:58:30 +0530

    Added routes for movie and user

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 import express from "express";
 import cors from "cors";
 
+import movie from "./routes/movie";
+import user from "./routes/user";
+
 const app = express();
 
 app.use(cors());
+
+app.use("/movie", movie);
+app.use("/user", user);
 
 app.listen("3000", () => {
   console.log("Server is running on PORT 3000");
