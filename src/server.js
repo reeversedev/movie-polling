8786a08e64a4c4d126c79d7f0418fc2a7d29a4c7
Prateek Gogia <gogia.prateek@hotmail.com>
2018-10-23 08:20:33 +0530

    Added Successful mongo connection

--- src/server.js
+++ src/server.js
@@ -1,15 +1,30 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 
-import movie from "./routes/movie";
-import user from "./routes/user";
+import movie from "../routes/movie";
+import user from "../routes/user";
+
+import config from "../config";
+
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect(
+    config.mongo,
+    { useNewUrlParser: true }
+  )
+  .then(success => console.log("Connection to Database is successfull"))
+  .catch(error => console.log("Connection to Database is failed"));
+
+mongoose.Promise = global.Promise;
 
 const app = express();
 
 app.use(cors());
 
-app.use("/movie", movie);
-app.use("/user", user);
+// app.use("/movie", movie);
+// app.use("/user", user);
 
 app.listen("3000", () => {
   console.log("Server is running on PORT 3000");
