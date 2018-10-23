0cff5ed88fca6a12620c2a3aaba7e0f370caf9cf
Prateek Gogia <gogia.prateek@hotmail.com>
2018-10-23 07:39:13 +0530

    Removed mysql connection

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
+
 app.listen("3000", () => {
-  console.log("Server is running");
+  console.log("Server is running on PORT 3000");
 });
